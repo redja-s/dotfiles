6a1628b569328086e43697c2469faa26b86b4720
jared <jared_solano@protonmail.com>
2024-01-08 09:37:52 +0000

    updates to neovim, tmux configs

---
+++ nvim/lua/plugins/colourscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+
+    opts = {
+      transparent_mode = true,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}

a7566382bd6ca8ad358f35e925de5b22d164acc5
jared <jared_solano@protonmail.com>
2024-01-21 22:05:21 +0000

    remove existing neovim config

--- nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Remap Ctrl+W to close buffer and switch to the previous buffer
-vim.api.nvim_set_keymap("n", "<C-w>", ":bp<Bar>bd #<CR>", { noremap = true, silent = true })

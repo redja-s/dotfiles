d0552ff7eea5419602e761d8b7311928196461e4
jared <jared_solano@protonmail.com>
2024-01-21 22:07:27 +0000

    base nvim with colorscheme using gruvbox

---
+++ nvim/init.lua
@@ -0,0 +1,7 @@
+require("jared.options")
+require("jared.commands")
+require("jared.keymaps")
+require("jared.lazy")
+
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
---
+++ nvim/lua/plugins/colourscheme.lua
@@ -0,0 +1,7 @@
+local Plugin = {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = true,
+}
+
+return Plugin

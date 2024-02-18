729a70e3c25e9dd52a46ee538324378f4234e928
jared <jared_solano@protonmail.com>
2024-02-18 08:28:28 +0000

    changes to neovim

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,9 +1,52 @@
-local Plugin = {"williamboman/mason.nvim"}
+local Plugin = { "williamboman/mason.nvim" }
+
+Plugin.dependencies = {
+  "williamboman/mason-lspconfig.nvim",
+}
+
+function Plugin.config()
+  local mason = require("mason")
+  local mason_lspconfig = require("mason-lspconfig")
+
+  mason.setup({
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
+      },
+    },
+  })
+
+  mason_lspconfig.setup({
+    ensure_installed = {
+      "tsserver",
+      "html",
+      "cssls",
+      "tailwindcss",
+      "svelte",
+      "lua_ls",
+      "graphql",
+      "emmet_ls",
+      "prismals",
+      "pyright",
+      "clangd",
+      "bashls",
+      "jdtls",
+      "gradle_ls",
+      "helm_ls",
+      "yamlls",
+      "sqls",
+    },
+
+    automatic_installation = true,
+  })
+end
 
 Plugin.lazy = false
 
 Plugin.opts = {
-  ui = {border = "rounded"}
+  ui = { border = "rounded" }
 }
 
 return Plugin

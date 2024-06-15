c8a300602efb8099541215119100488bd3ea329a
jared <jtlss14@gmail.com>
2024-06-15 13:20:29 +0100

    update trouble

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -4,11 +4,11 @@ Plugin.dependencies = {
   "nvim-tree/nvim-web-devicons",
 }
 
-Plugin.cmd = { "TroubleToggle", "Trouble" }
+Plugin.cmd = { "Trouble" }
 Plugin.opts = { use_diagnostic_signs = true }
 Plugin.keys = {
-  { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",         desc = "Diagnostics (Trouble)" },
-  { "<leader>xX", "<cmd>Trouble toggle filter.buf=0<cr>",        desc = "Buffer Diagnostics (Trouble)" },
+  { "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>",         desc = "Diagnostics (Trouble)" },
+  { "<leader>xx", "<cmd>Trouble toggle filter.buf=0<cr>",        desc = "Buffer Diagnostics (Trouble)" },
   { "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
   { "<leader>xl", "<cmd>Trouble loclist toggle<cr>",             desc = "Location List (Trouble)" },
   { "<leader>xq", "<cmd>Trouble qflist toggle<cr>",              desc = "Quickfix List (Trouble)" },
